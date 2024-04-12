74ffd7158d4b190cc35e3ced88778256ab9e7b4f
Son200202 <thanhson200202@gmail.com>
2024-04-12 10:18:01 +0700

    Son add helper file for Album, Playlist

---
+++ app/src/main/java/com/example/applistenmusic/helpers/AlbumHelper.java
@@ -0,0 +1,22 @@
+package com.example.applistenmusic.helpers;
+
+import com.example.applistenmusic.models.Album;
+import com.example.applistenmusic.models.Song;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlbumHelper {
+    Song song;
+    Album album;
+
+    public List<Album> getAlbumByArtis() {
+        return new ArrayList<>();
+    }
+    public Album getAlbumByID() {
+        return album;
+    }
+    public Album getAlbum() {
+        return album;
+    }
+}
---
+++ app/src/main/java/com/example/applistenmusic/helpers/PlayListHelper.java
@@ -0,0 +1,21 @@
+package com.example.applistenmusic.helpers;
+
+import com.example.applistenmusic.models.PlayList;
+import com.example.applistenmusic.models.Song;
+
+public class PlayListHelper {
+    Song song;
+    PlayList playList;
+
+    public PlayList getAllPlayList() {
+        return playList;
+    }
+
+    public PlayList getPlayListByID() {
+        return playList;
+    }
+
+    public PlayList getPlayListByUserID() {
+        return playList;
+    }
+}
