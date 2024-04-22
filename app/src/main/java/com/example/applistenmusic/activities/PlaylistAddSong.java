e8d7cb0a800dd5f82f66fe968676743450bd3369
Ven-Toan <ventoan2612@gmail.com>
2024-04-22 14:29:12 +0700

    Toan add favorite icon

---
+++ app/src/main/java/com/example/applistenmusic/activities/PlaylistAddSong.java
@@ -0,0 +1,16 @@
+package com.example.applistenmusic.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.applistenmusic.R;
+
+public class PlaylistAddSong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist_add_song);
+    }
+}
