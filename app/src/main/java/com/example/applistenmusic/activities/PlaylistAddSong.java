4cdf816729988721b2d8750a32a209fb9a2c4b48
Ven-Toan <ventoan2612@gmail.com>
2024-05-02 22:22:25 +0700

    Toan add new playlist

--- app/src/main/java/com/example/applistenmusic/activities/PlaylistAddSong.java
+++ app/src/main/java/com/example/applistenmusic/activities/PlaylistAddSong.java
@@ -24,11 +24,14 @@ import com.example.applistenmusic.interfaces.GenresLoadListener;
 import com.example.applistenmusic.models.Album;
 import com.example.applistenmusic.models.Artist;
 import com.example.applistenmusic.models.Genres;
+import com.example.applistenmusic.models.PlayList;
 import com.example.applistenmusic.models.Song;
 import com.example.applistenmusic.singletons.AlbumSingleton;
 import com.example.applistenmusic.singletons.ArtistSingleton;
 import com.example.applistenmusic.singletons.GenresSingleton;
 import com.example.applistenmusic.singletons.SongListSingleton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -42,15 +45,13 @@ import java.util.List;
 
 public class PlaylistAddSong extends AppCompatActivity {
     ImageView Feature, Home,Account, btnBack;
-    private EditText editText1, editText3, editText6;
-    AutoCompleteTextView editTextArtist ,editTextAlbum, editTextGenres ;
-    private Button buttonSave, buttonCancel, buttonUpload;
+    private EditText edtPlaylistName;
+    private Button buttonAdd, buttonCancel;
     RelativeLayout mainLayout;
     Spinner spinnerGenres, spinnerAlbum, spinnerArtist;
-    List<Genres> genres;
-    List<Album> albums;
-    List<Artist> artists;
     List<Song> songList;
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); // Lấy thông tin người dùng hiện tại
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,113 +69,6 @@ public class PlaylistAddSong extends AppCompatActivity {
             });
         }
 
-        if (AlbumSingleton.getInstance().hasAlbum()){
-            albums = AlbumSingleton.getInstance().getAllAlbumIfExist();
-        } else {
-            AlbumSingleton.getInstance().getAllAlbum(new AlbumLoadListener() {
-                @Override
-                public void onAlbumLoaded(List<Album> List) {
-                    albums = List;
-                }
-            });
-        }
-
-        if (ArtistSingleton.getInstance().hasArtist()){
-            artists = ArtistSingleton.getInstance().getAllArtistIfExist();
-        } else {
-            ArtistSingleton.getInstance().getAllArtist(new ArtistLoadListener(){
-                @Override
-                public void onArtistLoaded(List<Artist> List) {
-                    artists = List;
-                }
-            });
-        }
-
-        if (GenresSingleton.getInstance().hasGenres()){
-            genres = GenresSingleton.getInstance().getAllGenresIfExist();
-        } else {
-            GenresSingleton.getInstance().getAllGenres(new GenresLoadListener() {
-                @Override
-                public void onGenresLoaded(List<Genres> List) {
-                    genres = List;
-                }
-            });
-        }
-
-        List<String> genresToStringList = new ArrayList<>();
-        genresToStringList.add("");
-        for(Genres g : genres){
-            genresToStringList.add(g.getName());
-        }
-
-        ArrayAdapter<String> adapterGenres = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, genresToStringList);
-        adapterGenres.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinnerGenres.setAdapter(adapterGenres);
-
-        spinnerGenres.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                String selectedGenre = genresToStringList.get(position);
-                editTextGenres.setText(selectedGenre);
-                Toast.makeText(getApplicationContext(), "Selected Genre: " + selectedGenre, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // Không có gì được chọn
-            }
-        });
-
-        List<String> albumToStringList = new ArrayList<>();
-        albumToStringList.add("");
-        for(Album album : albums){
-            albumToStringList.add(album.getName());
-        }
-
-        ArrayAdapter<String> adapterAlbum = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, albumToStringList);
-        adapterAlbum.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinnerAlbum.setAdapter(adapterAlbum);
-
-        spinnerAlbum.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                String selectedGenre = albumToStringList.get(position);
-                editTextAlbum.setText(selectedGenre);
-                Toast.makeText(getApplicationContext(), "Selected Genre: " + selectedGenre, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // Không có gì được chọn
-            }
-        });
-
-        List<String> artistToStringList = new ArrayList<>();
-        artistToStringList.add("");
-        for(Artist artist : artists){
-            artistToStringList.add(artist.getName());
-        }
-
-        ArrayAdapter<String> adapterArtist = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, artistToStringList);
-        adapterArtist.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinnerArtist.setAdapter(adapterArtist);
-
-        spinnerArtist.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                String selectedGenre = artistToStringList.get(position);
-                editTextArtist.setText(selectedGenre);
-                Toast.makeText(getApplicationContext(), "Selected Genre: " + selectedGenre, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // Không có gì được chọn
-            }
-        });
 
         mainLayout.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -186,10 +80,16 @@ public class PlaylistAddSong extends AppCompatActivity {
             }
         });
         // Xử lý khi nhấn nút Save
-        buttonSave.setOnClickListener(new View.OnClickListener() {
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 saveData();
+                Toast.makeText(PlaylistAddSong.this, "Playlist added successfully", Toast.LENGTH_SHORT).show();
+                Intent playIntent = new Intent(PlaylistAddSong.this, SearchPlayList.class);
+                startActivity(playIntent);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                finish();
             }
         });
 
@@ -198,14 +98,6 @@ public class PlaylistAddSong extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 cancelOperation();
-            }
-        });
-
-        // Xử lý khi nhấn nút Upload
-        buttonUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                uploadFile();
             }
         });
 
@@ -221,65 +113,54 @@ public class PlaylistAddSong extends AppCompatActivity {
         });
     }
 
-    // Phương thức lấy dữ liệu từ các EditText và hiển thị thông báo
     private void saveData() {
-
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("song");
+        String userId = user.getUid(); // replace with actual user ID
+        DatabaseReference userPlaylistsRef = database.getReference("playList").child(userId);
 
-        // Tạo một song mới và đặt giá trị của nó vào Realtime Firebase
-        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        // Get the playlist name entered by the user
+        String playlistName = edtPlaylistName.getText().toString();
+
+        // Create a new Playlist object
+        PlayList playlist = new PlayList();
+        playlist.setName(playlistName);
+        List<Integer> songIdList = new ArrayList<>();
+        songIdList.add(0); // Add a child "1" with value "0"
+        playlist.setSongIdList(songIdList);
+        playlist.setUser(userId);
+
+        // Generate a unique key for the new child node
+        userPlaylistsRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 long count = dataSnapshot.getChildrenCount();
+                String newPlaylistId = String.valueOf(count + 1);
+                playlist.setId(Integer.parseInt(newPlaylistId));
 
-                // Tạo một child mới với ID tăng dần từ 1
-                String newSongId = String.valueOf(count + 1);
-
-                Song song = new Song();
-                song.setImage(editText3.getText().toString());
-                song.setName(editText1.getText().toString());
-                song.setAlbum(spinnerAlbum.getSelectedItemPosition());
-                song.setArtist(spinnerArtist.getSelectedItemPosition());
-                song.setGenres(spinnerGenres.getSelectedItemPosition());
-                song.setId(Integer.valueOf(newSongId));
-                song.setUrl("");
-                song.setView(1);
-                song.setLyric("");
-                myRef.child(newSongId).setValue(song);
+                // Save the Playlist object to Firebase
+                userPlaylistsRef.child(newPlaylistId).setValue(playlist);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Xử lý lỗi nếu có
+                // Handle error
             }
         });
     }
 
     // Phương thức xử lý khi nhấn nút Cancel
     private void cancelOperation() {
-        Intent playIntent = new Intent(PlaylistAddSong.this, SongManagement.class);
+        Intent playIntent = new Intent(PlaylistAddSong.this, SearchPlayList.class);
         startActivity(playIntent);
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         finish();
     }
 
-    // Phương thức xử lý khi nhấn nút Upload
-    private void uploadFile() {
-        // Xử lý ở đây (ví dụ: hiển thị dialog upload file)
-        Toast.makeText(this, "Upload file", Toast.LENGTH_SHORT).show();
-    }
-
     private void setControl(){
-        editText1 = findViewById(R.id.editText1);
-        editTextAlbum = findViewById(R.id.editText2);
-        editText3 = findViewById(R.id.editText3);
-        editTextArtist = findViewById(R.id.editText4);
-        editTextGenres = findViewById(R.id.editText5);
+        edtPlaylistName = findViewById(R.id.edtPlaylistName);
 
-        buttonSave = findViewById(R.id.buttonSave);
+        buttonAdd = findViewById(R.id.buttonAdd);
         buttonCancel = findViewById(R.id.buttonCancel);
-        buttonUpload = findViewById(R.id.buttonUpLoad);
 
         spinnerGenres= findViewById(R.id.spinnerGenres);
         spinnerAlbum= findViewById(R.id.spinnerAlbum);
