1574d634fe4cccf5024e46e747de6b4238edf259
Son200202 <thanhson200202@gmail.com>
2024-05-04 09:15:25 +0700

    Son edit youtubeSearchView

--- app/src/main/java/com/example/applistenmusic/activities/SearchYoutube.java
+++ app/src/main/java/com/example/applistenmusic/activities/SearchYoutube.java
@@ -9,8 +9,11 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,6 +24,12 @@ import com.example.applistenmusic.interfaces.YouTubeAPI;
 import com.example.applistenmusic.models.VideoItem;
 import com.example.applistenmusic.models.YouTubeSearchResponse;
 import com.example.applistenmusic.models.YouTubeVideo;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,13 +42,18 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SearchYoutube extends AppCompatActivity {
 
+    private TextView textViewTitle, textViewChannelName;
     private EditText edtSearch;
     private Button btnSearch;
     private WebView webView;
-    private String videoId = "Vk5-c_v4gMU"; // Thay VIDEO_ID bằng id của video muốn phát
+    private String videoId = "F0B7HDiY-10"; // Thay VIDEO_ID bằng id của video muốn phát
     private RecyclerView recyclerView;
     private VideoAdapter adapter;
     private List<VideoItem> videoItems = new ArrayList<>();
+    private VideoItem videoItem;
+    private static final int RC_SIGN_IN = 1001;
+    private LinearLayout playVideoLayout;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +62,9 @@ public class SearchYoutube extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler_view);
         btnSearch = findViewById(R.id.searchButton);
         edtSearch = findViewById(R.id.searchEditText);
+        playVideoLayout = findViewById(R.id.layoutPlayVideo);
+        textViewChannelName =  findViewById(R.id.textViewChannel);
+        textViewTitle =  findViewById(R.id.textViewTitle);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new VideoAdapter(videoItems);
         recyclerView.setAdapter(adapter);
@@ -63,9 +80,17 @@ public class SearchYoutube extends AppCompatActivity {
         adapter.setOnItemClickListener(new VideoAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(String VideoId) {
-                videoId = VideoId;
-                String html = "<html><body><iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/" + videoId + "\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
+                playVideoLayout.setVisibility(View.VISIBLE);
+                for(VideoItem v : videoItems){
+                    if(v.getVideoId().equals(VideoId)){
+                        videoItem = v;
+                    }
+                }
+                textViewTitle.setText(videoItem.getTitle().toString());
+                textViewChannelName.setText(videoItem.getVideoId().toString());
+                String html = "<html><head><style>body, html { margin: 0; padding: 0; } iframe { width: 100%; height: 100%; }</style></head><body><iframe src=\"https://www.youtube.com/embed/" + videoItem.getVideoId() + "\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
                 webView.loadData(html, "text/html", "utf-8");
+
             }
         });
 
@@ -75,6 +100,45 @@ public class SearchYoutube extends AppCompatActivity {
                 searchYouTubeVideos(edtSearch.getText().toString());
             }
         });
+
+
+//
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestIdToken("37669953608-ts4oirtc74b16fpk1nc7uirvqsgqa74f.apps.googleusercontent.com")
+//                .requestEmail()
+//                .build();
+//        mGoogleSignInClient = GoogleSignIn.getClient(SearchYoutube.this, gso);
+//
+//        // Bắt đầu quy trình đăng nhập khi người dùng nhấn nút đăng nhập
+//        signIn();
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Handle the result from the sign-in intent
+        if (requestCode == RC_SIGN_IN) {
+            try {
+                // Get the signed-in account information from the intent
+                GoogleSignInAccount account = GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException.class);
+                if (account != null) {
+                    // Handle the signed-in account, e.g., display the display name
+                    String displayName = account.getDisplayName();
+                    Toast.makeText(this, "Welcome, " + displayName, Toast.LENGTH_SHORT).show();
+                    // Sign-in successful, you can proceed to perform other actions
+                } else {
+                    // Handle case when account is null
+                    Toast.makeText(this, "Sign-in failed: Account is null", Toast.LENGTH_SHORT).show();
+                }
+            } catch (ApiException e) {
+                // Sign-in failed
+                Toast.makeText(this, "Failed to sign in: " + e.getStatusCode(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 
